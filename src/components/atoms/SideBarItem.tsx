5beefab32a53c9aca1bd79c442088034e5552926
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-06-12 17:22:11 +0900

    fix: SideBarItemでliタグのpropsを受け付けるよう修正(#3)

--- src/components/atoms/SideBarItem.tsx
+++ src/components/atoms/SideBarItem.tsx
@@ -1,7 +1,7 @@
-import { VFC } from 'react';
+import { VFC, ComponentPropsWithRef } from 'react';
 import { css } from '@emotion/react';
 
-type Props = {
+type Props = ComponentPropsWithRef<'li'> & {
   text: string;
   selected?: boolean;
 };
