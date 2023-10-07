bad2750c954798a9559b630f1bf41e6eb72fe9d9
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-10-07 17:40:41 +0900

    settings: tsconfig.jsonを最新のcreate-next-appテンプレベースにした(#10)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
-import { globalStyle } from '../styles/globals';
+import { globalStyle } from '@/styles/globals';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
